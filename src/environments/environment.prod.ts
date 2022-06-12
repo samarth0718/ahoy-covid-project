e8e73ebaf31290f3ed37945f63d935bdd1388602
samarth1118 <samarth@northladder.com>
2022-06-12 17:43:01 +0400

    added env variable to prod file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
 export const environment = {
   production: true,
-  xRapidApiHost: "covid-193.p.rapidapi.com",
-  xRapidApiKey: "e69e48f144mshdb436ee9bc13888p15c7adjsn318d220c934d"
+  server: "https://covid-193.p.rapidapi.com",
+  xRapidApiHost: "x-rapidapi-host",
+  xRapidApiHostValue: "covid-193.p.rapidapi.com",
+  xRapidApiKey: "x-rapidapi-key",
+  xRapidApiKeyValue: "e69e48f144mshdb436ee9bc13888p15c7adjsn318d220c934d"
 };
